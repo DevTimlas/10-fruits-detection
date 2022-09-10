151a627c2032aa008b8081eba8605f2d51c18f59
Timilehin <timilehinoladejo@yahoo.com>
2022-09-10 12:41:50 +0100

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -7,7 +7,7 @@ st.title("FruitsFood Detection with TF")
 st.header("Up to 10 classes of fruits Detection Test with Tensroflow")
 st.text("Upload a Fruit Image to detect and see it's contents.")
 st.text("Model not 99% accurate")
-RESULT_FOLDER = './10-fruits-detection/'
+RESULT_FOLDER = '/'
 
 
 def make_predict(img_pth):
@@ -23,7 +23,7 @@ def make_predict(img_pth):
         st.write(v)
     # st.write(f"{info2.name.to_string(index=False)} \n")
     results.save(RESULT_FOLDER)
-    predicted_image = Image.open('./10-fruits-detection/image0.jpg')
+    predicted_image = Image.open('./image0.jpg')
     st.image(predicted_image, caption="Predicted Image", use_column_width=False)
     # st.write(results.pandas().xyxy[0].to_json(orient='records'))
 
